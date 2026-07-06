265245021e2da567965b79aecdae441bec2c1516
sdadwdawfafaxc <jrjj1112jr@gmail.com>
2026-07-07 04:21:29 +0700

    Delete js directory

--- js/bot.js
+++
@@ -1,9 +0,0 @@
-// Client-side bot simulator disabled.
-window.botSimulator = {
-    start() {
-        // Disabled bot bidding simulation as requested.
-    },
-    stop() {
-        // No-op
-    }
-};
